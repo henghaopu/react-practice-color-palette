feaf933ea42b4acbc71b4a29e17540563b6845a1
Heng-Hao Pu <hpu@oswego.edu>
2020-06-06 21:07:27 -0400

    Test ColorBox animation by using hover

--- client/src/components/ColorBox.js
+++ client/src/components/ColorBox.js
@@ -3,6 +3,14 @@ import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copied: false,
+    };
+  }
+
   render() {
     const { backgroundColor, name } = this.props;
     return (
@@ -11,13 +19,17 @@ class ColorBox extends Component {
           <CopyToClipboard text={backgroundColor}>
             <button className='copy-button'>Copy</button>
           </CopyToClipboard>
+
           <div className='box-content'>
             <span tabIndex='0'>{name}</span>
           </div>
         </div>
+
         <span className='see-more' tabIndex='0'>
           More
         </span>
+
+        <div className='copy-overlay' style={{ backgroundColor }}></div>
       </div>
     );
   }
